20b42458ec3645eb99b41e5da3b247e1d28dc8f0
Ekta Ghosh <120678788+ektaghosh20@users.noreply.github.com>
2024-02-12 21:08:25 +0530

    Add files via upload

---
+++ starbucks/index.js
@@ -0,0 +1,8 @@
+function imgSlider(imgUrl){
+    document.querySelector('starbucks').src = imgUrl;
+
+}
+function changeCircleColor(color){
+    const circle=document.querySelector('.circle');
+    circle.style.background= color;
+}
